e435f26d500a6507a175a314c1764afe7b132ebe
DKarandikar <daniel.karandikar@zoho.com>
2020-12-09 07:40:56 +0000

    Do day9

--- src/days/Day9.hs
+++ src/days/Day9.hs
@@ -1,7 +1,42 @@
 module Day9 where
 
+preable :: Int
+preable = 25
+
 day9part1 :: IO ()
-day9part1 = putStrLn "TODO"
+day9part1 = do
+    input <- readFile "input/day9input.txt"
+    let ls = map (\x -> read x :: Int) (lines input)
+    print $ getInvalidNumber ls
 
 day9part2 :: IO ()
-day9part2 = putStrLn "TODO"
+day9part2 = do
+    input <- readFile "input/day9input.txt"
+    let ls = map (\x -> read x :: Int) (lines input)
+    print $ doDay9Part2 ls
+
+
+getInvalidNumber:: [Int] -> Int
+getInvalidNumber ls = ls!!(preable + head (dropWhile (isValid ls) [0..length ls]))
+
+isValid:: [Int] -> Int ->  Bool
+isValid nums x = (nums!!(x+preable)) `elem` sums 
+    where sums = [a + y | a <- take preable (drop x nums), y <- take preable (drop x nums), x /=y ]
+
+doDay9Part2 :: [Int] -> Int
+doDay9Part2 ls = 
+    let 
+        invalid = getInvalidNumber ls
+        start = head $ dropWhile (\x -> null (findRange ls x invalid)) [0.. length ls]
+        numbers = length $ findRange ls start invalid
+        contiguousRange = take (numbers-1) (drop start ls)
+    in
+        maximum contiguousRange + minimum contiguousRange
+
+findRange:: [Int] -> Int -> Int -> [Int]
+findRange input loc x = 
+    let 
+        nums = drop loc input
+        sums = takeWhile (<=x) [sum (take y nums)| y <- [0..]]
+    in
+        if last sums == x then sums else []
