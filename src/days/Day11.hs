d59a20dd1e78a1308d8be4ad4a1f723d6c0c5d7e
DKarandikar <daniel.karandikar@zoho.com>
2020-12-11 13:25:11 +0000

    Day11: Add some types

--- src/days/Day11.hs
+++ src/days/Day11.hs
@@ -5,7 +5,11 @@ import Data.Maybe ( fromMaybe )
 
 import Utils ( runTilEqual )
 
-type SeatMap = M.Map (Int, Int) Char
+
+data SeatLocState = Occupied | Empty | Floor | Invalid deriving Eq
+type Coord = (Int, Int)
+type SeatMap = M.Map Coord SeatLocState
+
 
 day11part1 :: IO ()
 day11part1 = do
@@ -23,59 +27,64 @@ day11part2 = do
     print $ sum $ [getSeatOccupied z finalState | z <- M.keys finalState]
 
 
-handleLine:: String -> Int -> SeatMap
-handleLine line lineNo = M.fromList [((x, lineNo), line!!x) | x <- [0.. length line - 1]]
+charToState:: Char -> SeatLocState
+charToState c 
+    | c == '.'  = Floor
+    | c == 'L'  = Empty
+    | c == '#'  = Occupied
+    | c == 'B'  = Invalid
+
 
 getSeatMap :: String -> SeatMap
-getSeatMap input = M.fromList [((x, lineNo), (lines input!!lineNo)!!x) | lineNo <- [0 .. length (lines input) -1], x <- [0.. length (lines input!!lineNo) - 1]]
+getSeatMap input = M.fromList [((x, lineNo), charToState $ (lines input!!lineNo)!!x) | lineNo <- [0 .. length (lines input) -1], x <- [0.. length (lines input!!lineNo) - 1]]
 
 
-applyStep :: ((Int, Int) -> SeatMap -> ((Int, Int), Char)) -> SeatMap -> SeatMap
+applyStep :: (Coord -> SeatMap -> (Coord, SeatLocState)) -> SeatMap -> SeatMap
 applyStep stepFn map = M.fromList [stepFn pos map | pos <- M.keys map]
 
-getSeatOccupied :: (Int, Int) -> SeatMap -> Int
+getSeatOccupied :: Coord -> SeatMap -> Int
 getSeatOccupied z smap = 
     case M.lookup z smap of
-        Just c -> if c == '#' then 1 else 0
+        Just c -> if c == Occupied then 1 else 0
         Nothing -> 0 
 
-getSeatState :: (Int, Int) -> SeatMap -> Char
-getSeatState z smap = fromMaybe 'B' (M.lookup z smap)
+getSeatState :: Coord -> SeatMap -> SeatLocState
+getSeatState z smap = fromMaybe Invalid (M.lookup z smap)
 
 
-dirs :: [(Int, Int)]
+dirs :: [Coord]
 dirs = [(x', y') | x' <- [-1, 0, 1], y' <- [-1, 0, 1], x' /= 0 || y'/= 0]
 
-getNextState :: (Int, Int) -> SeatMap -> ((Int, Int), Char)
+getNextState :: Coord -> SeatMap -> (Coord, SeatLocState)
 getNextState (x, y) smap = 
     let 
         adjacentOccupied = sum [getSeatOccupied (x + x', y + y') smap | (x', y') <- dirs]
         currentState = getSeatState (x, y) smap
         res
-            | currentState == '.' = '.'
-            | (currentState == 'L' && adjacentOccupied == 0) = '#'
-            | (currentState == '#' && adjacentOccupied >= 4) = 'L'
+            | currentState == Floor = Floor
+            | (currentState == Empty && adjacentOccupied == 0) = Occupied
+            | (currentState == Occupied && adjacentOccupied >= 4) = Empty
             | otherwise = currentState
     in 
         ((x, y), res)
 
 
-getNextStateP2 :: (Int, Int) -> SeatMap -> ((Int, Int), Char)
+getNextStateP2 :: Coord -> SeatMap -> (Coord, SeatLocState)
 getNextStateP2 (x, y) smap = 
     let 
         adjacentOccupied = sum [getSeatOccupied z smap | z <- getVisible (x, y) smap]
         currentState = getSeatState (x, y) smap
         res
-            | currentState == '.' = '.'
-            | (currentState == 'L' && adjacentOccupied == 0) = '#'
-            | (currentState == '#' && adjacentOccupied >= 5) = 'L'
+            | currentState == Floor = Floor
+            | (currentState == Empty && adjacentOccupied == 0) = Occupied
+            | (currentState == Occupied && adjacentOccupied >= 5) = Empty
             | otherwise = currentState
     in 
         ((x, y), res)
 
-getVisible :: (Int, Int) -> SeatMap -> [(Int, Int)]
+getVisible :: Coord -> SeatMap -> [Coord]
 getVisible (x, y) smap =  
         let 
-            vs = [ head $ dropWhile ((== '.') . (`getSeatState` smap)) [(x + n*x', y + n*y') | n <- [1..]] | (x', y') <- dirs]
+            vs = [ head $ dropWhile ((== Floor) . (`getSeatState` smap)) [(x + n*x', y + n*y') | n <- [1..]] | (x', y') <- dirs]
         in 
-            filter (\z-> getSeatState z smap /= 'B') vs   
+            filter (\z-> getSeatState z smap /= Invalid) vs   
