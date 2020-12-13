e9afae87a133f65ce157f78d84552707f880167b
DKarandikar <daniel.karandikar@zoho.com>
2020-12-13 15:44:33 +0000

    Refactor day13 a bit

--- src/days/Day13.hs
+++ src/days/Day13.hs
@@ -7,41 +7,30 @@ day13part1 :: IO ()
 day13part1 = do
     input <- readFile "input/day13input.txt"
     let t = read (head $ lines input) :: Int
-    let buses = split (==',') (lines input !! 1 )
+    let buses = filter (/= "x") $ split (==',') (lines input !! 1 )
     let res = foldl (handleLine t) (100000000, 100000000) buses
     print $ uncurry (*) res
 
 handleLine :: Int -> (Int, Int) -> String -> (Int, Int)
-handleLine x valSoFar busId = 
-    if busId == "x" then valSoFar else
-        let 
-            value = read busId :: Int
-            firstTime = head $ dropWhile (<x) [a*value | a <- [1..]]
-            -- bestIdSoFar = fst valSoFar
-            bestValueSoFar = snd valSoFar
-            difference = firstTime - x
-        in 
-            if difference < bestValueSoFar then 
-                (value, difference)
-            else
-                valSoFar
+handleLine t valSoFar busId 
+    | difference < bestValueSoFar = (value, difference)
+    | otherwise = valSoFar
+    where
+        value = read busId :: Int
+        firstTime = head $ dropWhile (<t) [a*value | a <- [1..]]
+        bestValueSoFar = snd valSoFar
+        difference = firstTime - t
+        
 
 day13part2 :: IO ()
 day13part2 = do
     input <- readFile "input/day13input.txt"
-    let buses = split (==',') (lines input !! 1 )
-    let constraints = filter (/= (0,0)) $ map (getCons buses) [0..length buses - 1]
-    print constraints
-    
+    let constraints = getContraints $ split (==',') (lines input !! 1 )
     print $ crt constraints
 
-getCons :: [String] -> Int -> (Integer ,Integer)
-getCons buses id = 
-    let 
-        busID = buses !! id
-    in 
-        if busID == "x" then (0,0) else
-            let 
-                val = read busID :: Int
-            in 
-                (toInteger (val - id), toInteger val)
+getContraints :: [String] -> [(Integer, Integer)]
+getContraints buses = [ (toInteger (val - index), toInteger val) 
+    | index <- [0..length buses - 1]
+    , buses !! index /= "x"
+    , let val = read (buses !! index) :: Int
+    ]
